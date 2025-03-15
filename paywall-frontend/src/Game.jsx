45b2c5c2d00cfa9f80fa7db6dcd627ce1e739d35
Clyoth <sumiplorem@gmail.com>
2025-03-16 03:33:43 +0800

    Frontend paywall setup complete

--- paywall-frontend/src/Game.jsx
+++ paywall-frontend/src/Game.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="justify-center items-center flex h-screen w-screen">
+      <h1 className="text-4xl text-center">WEBGL TO BE IMPLEMENTED HERE</h1>
+    </div>
+  );
 };
 
 export default Game;
--- paywall-frontend/src/Home.jsx
+++ paywall-frontend/src/Home.jsx
@@ -27,18 +27,30 @@ const Home = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className="bg-slate-50 w-screen h-screen flex items-center justify-center">
+      <form
+        className="flex flex-col p-6 rounded-md drop-shadow-2xl bg-white"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-center font-bold text-slate-800">
+          Enter the code you received{" "}
+        </h1>
         <input
           type="text"
           value={code}
           onChange={(e) => setCode(e.target.value)}
           placeholder="Enter Code"
+          className="border-1 text-center text-slate-800 border-slate-600 rounded-sm my-4"
         />
-        <button type="submit">Submit</button>
+        <button
+          className="text-center bg-blue-600 text-slate-900 px-6 py-1 rounded-sm my-2 w-fit self-center"
+          type="submit"
+        >
+          Submit
+        </button>
+        {message && <p className="text-red-600 self-center">{message}</p>}
       </form>
-      {message && <p>{message}</p>}
-    </>
+    </div>
   );
 };
 
